a1067a72eb89cbbbe7c09d36ef3e7163ef647392
한창윤 <hansyooni11@gmail.com>
2024-01-05 15:38:27 +0900

    build like
    build like

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -3,9 +3,11 @@ import { LikeService } from "./like.service";
 import { LikeController } from "./like.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Like } from "./entity/like.entity";
+import { MemberModule } from "src/member/member.module";
+import { CommentModule } from "src/comment/comment.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Like])],
+  imports: [TypeOrmModule.forFeature([Like]), MemberModule, CommentModule],
   providers: [LikeService],
   controllers: [LikeController],
 })
