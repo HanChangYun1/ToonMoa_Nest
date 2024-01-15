ae724c4f150d0bd3f45d51721662d31d97d26298
HanChangYun1 <hansyooni11@gmail.com>
2024-01-15 12:38:29 +0900

    set config
    set config

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 4000;
   app.enableCors({
     credentials: true,
   });
-  await app.listen(4000);
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
